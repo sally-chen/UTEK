d1db589d95fe41aa2246c26d3fd1cc484b31b9f4
sally-chen <chuhan.chen@mail.utoronto.ca>
2017-01-21 14:16:23 -0500

    pairs object

---
+++ utek/Pair.h
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   pair.h
+ * Author: sally090230
+ *
+ * Created on January 21, 2017, 1:55 PM
+ */
+
+#ifndef PAIR_H
+#define PAIR_H
+class Pair{
+    double lattitude;
+    double longitude;
+}
+
+
+#endif /* PAIR_H */
+
