d47091f8e2c2c82389cb0ba1581c9403524a2816
sally-chen <chuhan.chen@mail.utoronto.ca>
2017-01-21 13:31:59 -0500

    created project

---
+++ utek/main.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: sally090230
+ *
+ * Created on January 21, 2017, 1:31 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
